96fef4a6b1b7f9729aa57b8faa230053bdd67c99
RyanYang99 <diddbstjd0220@naver.com>
2024-05-30 17:28:54 +0900

    ex4 문제 완료

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,37 @@ import React, { useState, useEffect } from "react";
 
 // TODO: useFetch 라는 커스텀훅을 사용해서 리팩터링 해보세요
 
-const App = () => {
-  const [title, setTitle] = useState(null);
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/todos/1",
-        );
+        const response = await fetch(url);
         console.log("response:", response);
         if (!response.ok) {
           throw new Error("Network 오류");
         }
         const result = await response.json();
-        setTitle(result.title);
+        setData(result);
       } catch (error) {
         setError(error);
       } finally {
         setLoading(false);
       }
     };
-
     fetchData();
-  }, []);
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+const App = () => {
+  const { data, loading, error } = useFetch(
+    "https://jsonplaceholder.typicode.com/todos/1"
+  );
 
   if (loading) return <h1>Loading...</h1>;
   if (error) return <h1>Error: {error.message}</h1>;
@@ -35,7 +40,7 @@ const App = () => {
   return (
     <div>
       <h1>Data Fetching Example</h1>
-      <p>{title}</p>
+      <p>{data.title}</p>
     </div>
   );
 };
